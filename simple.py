d59ba637e2ec304f814c5317f2e9cf7fa19569dc
sterling_hebert <sterling_hebert@outlook.com>
2023-05-02 17:39:38 -0500

    added explaination

--- simple.py
+++ simple.py
@@ -1,3 +1,24 @@
+# helpful commands:
+
+# installer:
+# pipenv install Flask~=2.2.2
+
+# version checker:
+#pipenv run flask --version
+
+# starting application:
+# pipenv run flask run or pipenv run flask run -p 5001
+
+# flask installation:
+# pipenv install python-dotenv~=0.21
+
+# file server specification (setting env file):
+# export FLASK_APP=simple.py
+
+# removing env variables:
+# unset FLASK_APP
+# unset FLASK_DEBUG
+
 from flask import Flask
 # Load configuration class
 from config import Config
@@ -25,3 +46,27 @@ def hello():
 # def hello():
 #     # Use configuration variable
 #     return f'<h1>{app.config["greeting"]}</h1>'
+
+# seperate routes
+@app.route('/about')
+def about():
+    return '<h1>About</h1>'
+
+# wildcrad routes
+@app.route('/item/<id>')
+def item(id):
+    return f'<h1>Item {id}</h1>'
+
+@app.before_request
+def before_request_function():
+    print("before_request is running")
+
+
+@app.after_request
+def after_request_function(response):
+    print("after_request is running")
+    return response
+
+@app.before_first_request
+def before_first_function():
+    print("before_first_request happens once")
